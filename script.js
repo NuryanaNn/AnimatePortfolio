12c45b6e8033f2b691f6f1cc241c8aff264017fc
NuryanaNn <nuryan504@gmail.comescgit clear>
2021-09-17 18:46:43 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    // Navbar
+    $("#nav").addClass("animate__animated animate__backInDown animate__delay-3s");
+
+
+    // Gambar html, css, bootstrap, gihub, js
+    $("#jumbo").addClass("animate__animated animate__slideInLeft animate__delay-1s");
+    $("#box").addClass("animate__animated animate__bounceInDown animate__delay-2s");
+    $("#box2").addClass("animate__animated animate__bounceInDown animate__delay-2s");
+    $("#box3").addClass("animate__animated animate__bounceInDown animate__delay-2s");
+    $("#box4").addClass("animate__animated animate__bounceInDown animate__delay-3s");
+    $("#box5").addClass("animate__animated animate__bounceInDown animate__delay-3s");
+    $("#box6 ").addClass("animate__animated animate__bounceInDown animate__delay-3s");
+    $("#box7 ").addClass("animate__animated animate__bounceInDown animate__delay-4s");
+    $("#box8 ").addClass("animate__animated animate__bounceInDown animate__delay-4s");
+    $("#box9 ").addClass("animate__animated animate__bounceInDown animate__delay-4s");
+    $("#box10 ").addClass("animate__animated animate__bounceInDown animate__delay-4s");
+
+    // Find Me
+    $("#find").addClass("animate__animated animate__lightSpeedInLeft animate__delay-4s");
+});
